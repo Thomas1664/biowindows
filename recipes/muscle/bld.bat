0d77a02f7652ba4b0d367a2c4c629c638cc4bd80
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2025-10-15 14:39:13 +0200

    Download file from build script

--- recipes/muscle/bld.bat
+++ recipes/muscle/bld.bat
@@ -1,4 +1,22 @@
 @echo on
 setlocal enabledelayedexpansion
-mkdir %PREFIX%\Scripts
-copy %SRC_DIR%\muscle-win64.v5.3.exe %PREFIX%\Scripts\muscle.exe
+
+rem URL and expected SHA256
+set "URL=https://github.com/rcedgar/muscle/releases/download/v5.3/muscle-win64.v5.3.exe"
+set "SHA256_EXPECTED=  sha256: a86637db99deb3efa1cb6762c4e3160ca33644608270eb167e4214f1f9409711"
+
+rem Download the binary
+powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%SRC_DIR%\muscle.exe'"
+
+rem Compute SHA256 hash
+for /f "usebackq tokens=1" %%a in (`powershell -Command "Get-FileHash -Algorithm SHA256 '%SRC_DIR%\muscle.exe' | Select-Object -ExpandProperty Hash"`) do set SHA256_ACTUAL=%%a
+
+rem Verify hash
+if /i not "%SHA256_ACTUAL%"=="%SHA256_EXPECTED%" (
+    echo SHA256 mismatch!
+    exit /b 1
+)
+
+rem Copy to environment Scripts folder
+mkdir "%PREFIX%\Scripts"
+copy "%SRC_DIR%\muscle.exe" "%PREFIX%\Scripts\muscle.exe"
